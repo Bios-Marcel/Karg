22cdabb8d995e0c260b7fa74f64bb28a60a53ff6
Bios-Marcel <marceloschr@googlemail.com>
2018-07-27 17:37:27 +0200

    Remove test failure

--- src/test/kotlin/com/msc/karg/TestHelloPrinter.kt
+++ src/test/kotlin/com/msc/karg/TestHelloPrinter.kt
@@ -4,14 +4,9 @@ class TestHelloPrinter : Test() {
 
     override fun registerTestFunctions() {
         registerTestFunction(this::testHelloPrinter)
-        registerTestFunction(this::testFailure)
     }
 
     fun testHelloPrinter() {
         assertEquals("Hello", "Hello");
-    }
-
-    fun testFailure() {
-        fail()
     }
 }
